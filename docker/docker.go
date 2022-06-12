189eb0098b189c4505764d6b694374d8fc3b5a9b
Derek Ray <derek9ray@gmail.com>
2022-06-12 20:18:21 +0800

    add container list and container inspect implement for docker

--- docker/docker.go
+++ docker/docker.go
@@ -2,8 +2,13 @@ package docker
 
 import (
 	"context"
+	"fmt"
 	"github.com/derekhjray/containers/types"
+	dtypes "github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	log "github.com/sirupsen/logrus"
+	"strings"
+	"time"
 )
 
 func New() (types.Runtime, error) {
@@ -52,11 +57,92 @@ func (d *docker) ImageInspect(ctx context.Context, imageId string) (*types.Image
 }
 
 func (d *docker) ContainerList(ctx context.Context, option *types.ContainerListOption) ([]*types.Container, error) {
-	return nil, types.ErrMethodNotImplemented
+	list, err := d.Client.ContainerList(ctx, dtypes.ContainerListOptions{All: option.All})
+	if err != nil {
+		return nil, err
+	}
+
+	containers := make([]*types.Container, 0, len(list))
+	for index := range list {
+		container := &types.Container{
+			ID:      list[index].ID,
+			Image:   list[index].Image,
+			ImageID: list[index].ImageID,
+			State:   list[index].Status,
+			Labels:  list[index].Labels,
+			NetMode: list[index].HostConfig.NetworkMode,
+			Created: time.Unix(list[index].Created, 0),
+		}
+
+		if len(list[index].Names) > 0 {
+			container.Name = strings.TrimPrefix(list[index].Names[0], "/")
+		}
+
+		containers = append(containers, container)
+	}
+
+	return containers, nil
 }
 
 func (d *docker) ContainerInspect(ctx context.Context, containerId string) (*types.Container, error) {
-	return nil, types.ErrMethodNotImplemented
+	cjson, err := d.Client.ContainerInspect(ctx, containerId)
+	if err != nil {
+		return nil, err
+	}
+
+	container := &types.Container{
+		ID:    cjson.ID,
+		Name:  strings.TrimPrefix(cjson.Name, "/"),
+		Image: cjson.Image,
+	}
+
+	if container.Created, err = time.Parse(time.RFC3339Nano, cjson.Created); err != nil {
+		log.Debug("Parse container(%s) created time failed, reason: %v", container.Name, err)
+	}
+
+	if cjson.State != nil {
+		if container.Started, err = time.Parse(time.RFC3339Nano, cjson.State.StartedAt); err != nil {
+			log.Debug("Parse container(%s) started time failed, reason: %v", container.Name, err)
+		}
+
+		if container.Finished, err = time.Parse(time.RFC3339Nano, cjson.State.FinishedAt); err != nil {
+			log.Debug("Parse container(%s) finished time failed, reason: %v", container.Name, err)
+		}
+
+		container.State = cjson.State.Status
+		container.Pid = cjson.State.Pid
+	}
+
+	if cjson.Config != nil {
+		container.Envs = cjson.Config.Env
+		container.Labels = cjson.Config.Labels
+		container.User = cjson.Config.User
+		if container.User == "" {
+			container.User = "root"
+		}
+	}
+
+	if cjson.HostConfig != nil {
+		container.Privileged = cjson.HostConfig.Privileged
+		container.NetMode = string(cjson.HostConfig.NetworkMode)
+		container.PidMode = string(cjson.HostConfig.PidMode)
+		container.Memory = cjson.HostConfig.Memory
+		container.CPUs = fmt.Sprintf("%0.2f", float64(cjson.HostConfig.CPUQuota)/float64(cjson.HostConfig.CPUPeriod))
+	}
+
+	if cjson.NetworkSettings != nil {
+		container.IPAddress = cjson.NetworkSettings.IPAddress
+		if container.IPAddress == "" {
+			for _, settings := range cjson.NetworkSettings.Networks {
+				container.IPAddress = settings.IPAddress
+				if container.IPAddress != "" {
+					break
+				}
+			}
+		}
+	}
+
+	return container, nil
 }
 
 func (d *docker) ContainerStart(ctx context.Context, containerId string) error {
--- types/container.go
+++ types/container.go
@@ -1,4 +1,24 @@
 package types
 
+import "time"
+
 type Container struct {
+	ID         string
+	Name       string
+	Image      string
+	ImageID    string
+	State      string
+	Pid        int
+	PidMode    string
+	NetMode    string
+	Envs       []string
+	Labels     map[string]string
+	User       string
+	Privileged bool
+	Memory     int64
+	CPUs       string
+	IPAddress  string
+	Created    time.Time
+	Started    time.Time
+	Finished   time.Time
 }
